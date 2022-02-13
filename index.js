2f3aa21a49b516e0abfc53a97d4759d39f028967
ano201 <ahmu689@gmail.com>
2022-02-14 04:51:22 +0600

    dynamic book system

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const fileUpload = require("express-fileupload");
 const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 5000;
+const ObjectId = require("mongodb").ObjectId;
 
 app.use(cors());
 app.use(express.json());
@@ -74,6 +75,13 @@ async function run() {
     app.get("/books", async (req, res) => {
       const cursor = booksCollection.find({});
       const books = await cursor.toArray();
+      res.json(books);
+    });
+
+    app.get("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const books = await booksCollection.findOne(query);
       res.json(books);
     });
 
